40d1faee1a5bde49de6a4c7914dbc22a0585c492
Haley Whitman <53501096+camelops@users.noreply.github.com>
2021-03-27 14:13:13 -0400

    part1b step 4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,24 +2,27 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Footer exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   )
 }
@@ -31,19 +34,32 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+
+
+  let partsList = []
+  props.parts.forEach((item, index) => {
+    partsList.push( <Part key={index} part={item}/>)
+  })
+  
   return (
     <>
-      <Part part={props.part1}/>
-      <Part part={props.part2}/>
-      <Part part={props.part3}/>
+      {partsList}
     </>
+
   )
 }
 
-const Footer = (props) => {
+const Total = (props) => {
+
+  let total = 0;
+  props.parts.forEach((item, index) => {
+    total += item.exercises;
+  })
+
+
   return (
     <p>
-      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      Number of exercises {total}
     </p>
   )
 }
