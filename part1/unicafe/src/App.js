6bf3a51f53279e271b445f7a077d3f6e56b431a4
Haley Whitman <53501096+camelops@users.noreply.github.com>
2021-03-27 03:12:24 -0400

    part1b step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,19 +2,24 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} 
-                exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-      <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Footer exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </>
   )
 }
@@ -28,9 +33,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <Part part={props.part1} exercise={props.exercises1}/>
-      <Part part={props.part2} exercise={props.exercises2}/>
-      <Part part={props.part3} exercise={props.exercises3}/>
+      <Part part={props.part1}/>
+      <Part part={props.part2}/>
+      <Part part={props.part3}/>
     </>
   )
 }
@@ -46,7 +51,7 @@ const Footer = (props) => {
 const Part = (props) => {
   return (
     <p>
-      {props.part} {props.exercise}
+      {props.part.name} {props.part.exercises}
     </p>
   )
 }
