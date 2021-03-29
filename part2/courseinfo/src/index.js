8d1dce6bb563f5595c8f336f3b2d66fffe779963
Haley Whitman <53501096+camelops@users.noreply.github.com>
2021-03-29 15:04:33 -0400

    part2a courseinfo step10

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,49 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Course from './components/Course'
 
-const Header = ({ name }) => {
-  console.log(name);
-  return (
-    <h1>{name}</h1>
-  )
-}
 
-const Total = ({ parts }) => {
-  
-  const sum = parts.reduce((a, b) => a + (b['exercises'] || 0), 0)
-
-  return(
-    <p><b>Total of {sum} exercises</b></p>
-  ) 
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => 
-        <Part key={part.id} part={part} />
-        )}
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
 
 const Courses = ({courses}) => {
   return (
