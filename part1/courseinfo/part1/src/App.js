ca71c18f8ec95676ebe802a592064b11dab709dd
Haley Whitman <53501096+camelops@users.noreply.github.com>
2021-03-28 20:24:27 -0400

    part1d

--- part1/courseinfo/part1/src/App.js
+++ part1/courseinfo/part1/src/App.js
@@ -1,31 +1,45 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
   
-  const setToZero = () => setCounter(0)
 
   return (
-    <>
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
       <div>
-        <Display counter={counter}/>
+        the app is used by pressing the buttons
       </div>
-      <Button
-        handleClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        handleClick={setToZero}
-        text='zero'
-      />
-      <Button
-        handleClick={decreaseByOne}
-        text='minus'
-      />
-   </>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
